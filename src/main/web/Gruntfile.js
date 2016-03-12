8f6af7e40f6032b8b9e88baed77480e9d5784c65
kaakaa <stooner.hoe@gmail.com>
2016-03-12 19:12:16 +0900

    fix building frontend packages

--- src/main/web/Gruntfile.js
+++ src/main/web/Gruntfile.js
@@ -3,21 +3,29 @@ module.exports = function(grunt) {
     browserify: {
       dist: {
         files: {
-          'src/main/resources/public/ppt-museum/js/pdf.js-controller.js': ['src/main/web/index.js']
+          'dist_packages/js/pdf.js-controller.js': ['src/main/web/index.js']
         }
       }
     },
     cssmin : {
       dist: {
         src: ['node_modules/pdf.js-controller/css/pdf-slide.css'],
-        dest: 'src/main/resources/public/ppt-museum/css/pdf-slide.min.css'
+        dest: 'dist_packages/css/pdf-slide.min.css'
+      }
+    },
+    wiredep: {
+      task: {
+        src: [
+          '../resources/templates/**/*.jade'
+        ]
       }
     }
   });
 
   grunt.loadNpmTasks("grunt-browserify");
   grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-wiredep");
 
   grunt.registerTask("default", "build");
-  grunt.registerTask("build", ["browserify", "cssmin"])
+  grunt.registerTask("build", ["browserify", "cssmin", "wiredep"])
 };
