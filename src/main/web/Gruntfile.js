423c251f000ba542f0e1f1fa82696b5a0fc47f8a
kaakaa <stooner.hoe@gmail.com>
2016-03-12 23:16:05 +0900

    add bower dependencies

--- src/main/web/Gruntfile.js
+++ src/main/web/Gruntfile.js
@@ -3,27 +3,66 @@ module.exports = function(grunt) {
     browserify: {
       dist: {
         files: {
-          'dist_packages/public/ppt-museum/js/pdf.js-controller.js': ['src/main/web/index.js']
+          'dist_packages/public/ppt-museum/pdfjs-controller/js/pdf.js-controller.js': ['index.js']
         }
       }
     },
-    cssmin : {
+    cssmin: {
       dist: {
         src: ['node_modules/pdf.js-controller/css/pdf-slide.css'],
-        dest: 'dist_packages/public/ppt-museum/css/pdf-slide.min.css'
+        dest: 'dist_packages/public/ppt-museum/pdfjs-controller/css/pdf-slide.min.css'
+      }
+    },
+    "bower-install-simple": {
+      options: {
+        directory: "components/public/ppt-museum"
+      },
+      "prod": {
+        options: {
+          production: true
+        }
+      },
+      "dev": {
+        options: {
+          production: false
+        }
+      }
+    }, 
+    bower: {
+      dev: {
+        base: 'components/public/ppt-museum',
+        dest: 'dist_packages/public/ppt-museum/',
+        options: {
+          paths: {
+            bowerDirectory: "components/public/ppt-museum"
+          }
+        }
       }
     },
     wiredep: {
       task: {
-        src: ['../resources/templates/**/*.jade']
+        src: ['../resources/templates/**/*.jade'],
+        directory: 'components/public/ppt-museum',
+        ignorePath: "../../web/components/public"
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'dist_packages/public/ppt-museum/pdfjs-dist/cmaps',
+        src: '*',
+        dest: 'dist_packages/public/ppt-museum/cmaps/'
       }
     }
   });
 
   grunt.loadNpmTasks("grunt-browserify");
   grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-bower-install-simple");
+  grunt.loadNpmTasks("main-bower-files");
   grunt.loadNpmTasks("grunt-wiredep");
+  grunt.loadNpmTasks("grunt-contrib-copy");
 
   grunt.registerTask("default", "build");
-  grunt.registerTask("build", ["browserify", "cssmin", "wiredep"]);
+  grunt.registerTask("build", ["browserify", "cssmin", "bower-install-simple", "bower", "copy", "wiredep"]);
 };
