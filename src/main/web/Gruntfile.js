38c2ba87a2554fa8b500492abd2201f08b6675b9
kaakaa <stooner.hoe@gmail.com>
2016-03-12 19:46:42 +0900

    fix

--- src/main/web/Gruntfile.js
+++ src/main/web/Gruntfile.js
@@ -3,21 +3,19 @@ module.exports = function(grunt) {
     browserify: {
       dist: {
         files: {
-          'dist_packages/js/pdf.js-controller.js': ['src/main/web/index.js']
+          'dist_packages/public/ppt-museum/js/pdf.js-controller.js': ['src/main/web/index.js']
         }
       }
     },
     cssmin : {
       dist: {
         src: ['node_modules/pdf.js-controller/css/pdf-slide.css'],
-        dest: 'dist_packages/css/pdf-slide.min.css'
+        dest: 'dist_packages/public/ppt-museum/css/pdf-slide.min.css'
       }
     },
     wiredep: {
       task: {
-        src: [
-          '../resources/templates/**/*.jade'
-        ]
+        src: ['../resources/templates/**/*.jade']
       }
     }
   });
@@ -27,5 +25,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-wiredep");
 
   grunt.registerTask("default", "build");
-  grunt.registerTask("build", ["browserify", "cssmin", "wiredep"])
+  grunt.registerTask("build", ["browserify", "cssmin", "wiredep"]);
 };
